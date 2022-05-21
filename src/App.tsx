f6792bfa34fb11a1ffe37750bc0a8e1550de66a7
batbrain9392 <batbrain9392@gmail.com>
2022-05-21 17:48:17 +0100

    debouncing input and filtering with regex

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import './App.css';
 import { Option } from './components';
 import { getManagerData } from './services';
 import { ManagerDisplayData } from './types';
+import debounce from 'lodash.debounce';
 
 const App = () => {
   const [managers, setManagers] = useState<ManagerDisplayData[]>([]);
+  const [filteredManagers, setFilteredManagers] = useState<typeof managers>([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    (async () => {
-      setManagers(await getManagerData());
-    })();
-
-    return () => {};
+    (async () => setManagers(await getManagerData()))();
   }, []);
 
+  const inputChangeHandler = useCallback(
+    (value: string) => {
+      const valueWithNoSpaces = value.replace(/\s/g, '');
+      if (!valueWithNoSpaces) setFilteredManagers([]);
+      else {
+        const searchRegex = new RegExp(valueWithNoSpaces, `i`);
+        const data = managers.filter(({ searchTerm }) => searchTerm.match(searchRegex));
+        setFilteredManagers(data);
+      }
+    },
+    [managers]
+  );
+
+  const debouncedInputChangeHandler = useMemo(() => debounce(inputChangeHandler, 300), [inputChangeHandler]);
+
+  useEffect(() => debouncedInputChangeHandler(searchTerm), [searchTerm, debouncedInputChangeHandler]);
+
   return (
-    <ul>
-      {managers.map((manager) => (
-        <Option key={manager.id} data={manager}></Option>
-      ))}
-    </ul>
+    <>
+      <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+      <ul>
+        {(filteredManagers.length ? filteredManagers : managers).map((manager) => (
+          <Option key={manager.id} data={manager}></Option>
+        ))}
+      </ul>
+    </>
   );
 };
 
