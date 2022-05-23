e4b34551a49640a0cce1ed2247f9c412193bbfb7
batbrain9392 <batbrain9392@gmail.com>
2022-05-23 10:17:07 +0100

    added test for the app comp

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+import { provideAutoSpy } from 'jasmine-auto-spies';
 import { AppComponent } from './app.component';
+import { GetApiDataService } from './services';
 
-// describe('AppComponent', () => {
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [AppComponent],
-//     }).compileComponents();
-//   });
+describe('AppComponent', () => {
+  let itemUnderTest: AppComponent;
 
-//   it('should create the app', () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.componentInstance;
-//     expect(app).toBeTruthy();
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [AppComponent, provideAutoSpy(GetApiDataService)],
+    });
+    itemUnderTest = TestBed.inject(AppComponent);
+  });
 
-//   it(`should have as title 'workday-assignment'`, () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.componentInstance;
-//     expect(app.title).toEqual('workday-assignment');
-//   });
-
-//   it('should render title', () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     fixture.detectChanges();
-//     const compiled = fixture.nativeElement as HTMLElement;
-//     expect(compiled.querySelector('.content span')?.textContent).toContain(
-//       'workday-assignment app is running!'
-//     );
-//   });
-// });
+  describe(`INIT`, () => {
+    it('should be created', () => {
+      expect(itemUnderTest).toBeTruthy();
+    });
+  });
+});
